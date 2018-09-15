00e0f9e6730e355f1deaf21b34fd3ce4665271fd
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-09-15 17:59:22 +0700

    a

--- api.js
+++ api.js
@@ -1,57 +1,34 @@
+
+/***************************/
+if (!self['console']) {
+    self.console = {};
+    self.console.info = function () { };
+    self.console.log = function (v1, v2) { self.postMessage({ Key: 'API_LOG', Value1: v1, Value2: v2 }); };
+    self.console.warn = function () { };
+    self.console.error = function () { };
+}
+
+var CACHE_URI = {};
+
 var API_ID = 'api-id-xxxxx-xxxx-4xxx-yxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
 });
 
-function load(url, type) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false);
-    xhr.send(null);
-    if (type == 'text') return xhr.responseText;
-    return JSON.parse(xhr.responseText);
-}
+self.addEventListener('message', function (e) {    
+    //console.log('API:', e.data);
 
-// #region [ LOG ]
+    var m = e.data;
+    if (m == null) return;
+    var input = m.Input;
 
-var WS_LOG, LOG_OPEN = false;
+    switch (m.Key) {
+        case API_KEY.CACHE_URI:
+            console.log('API: -> CACHE_URI:', input);
 
-if ('WebSocket' in self) {
-    WS_LOG = new WebSocket('ws://localhost:11111');
-    WS_LOG.onopen = function () { LOG_OPEN = true; f_log1(API_ID);  };
-    WS_LOG.onclose = function () { };
-    WS_LOG.onmessage = function (e) { if (_view) console.log(e.data); };
-}
-
-function f_log1(data) {
-    if (WS_LOG && data && LOG_OPEN) {
-        if (typeof data == 'string')
-            WS_LOG.send(data);
-        else
-            WS_LOG.send(JSON.stringify(data));
-    }
-}
-
-function f_log2(data1, data2) {
-    if (WS_LOG && LOG_OPEN) {
-        var s = '';
-        if (data1) { if (typeof data1 == 'string') s = data1; else s = JSON.stringify(data1); }
-        if (data2) { if (typeof data2 == 'string') s += ' ===== ' + data2; else s += ' ===== ' + JSON.stringify(data2); }
-        WS_LOG.send(s);
-    }
-}
-
-// #endregion
-
-self.addEventListener('message', function (e) {
-
-    self.postMessage({ 'API: ====> data: ': e.data });
-
-    switch (e.data) {
-        case 'CONNECT':
-            f_log1('API: UI -> CONNECTED ...');
             break;
         case 'TEST':
-            f_log1('API: UI -> CONNECTED ...');
+            console.log('API: UI -> CONNECTED ...');
 
             var blob = new Blob(['body { background-color: yellow; }'], { type: 'text/css' });
             self.postMessage(blob);
--- config.js
+++ config.js
@@ -1,4 +1,26 @@
+
 var CF = {
     //API_URL_SRC_JS_WORKER: 'https://raw.githubusercontent.com/git-thinh/appui/master/api.js',
+    API_URL_SRC_JS_UNDERSCORE: 'http://localhost:60000/underscore.min.js',
     API_URL_SRC_JS_WORKER: 'http://localhost:60000/api.js',
 };
+
+var URI_KEY = {
+    JS_WORKER: 'JS_WORKER',
+    JS_UNDERSCORE: 'URI_JS_UNDERSCORE',
+    JS_CONFIG: 'URI_JS_CONFIG',
+}
+
+var API_KEY = {
+    CACHE_URI: 'CACHE_URI',
+};
+
+
+function f_load(url, type) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send(null);
+    if (type == 'text') return xhr.responseText;
+    return JSON.parse(xhr.responseText);
+}
+
